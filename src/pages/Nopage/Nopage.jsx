6aa9949d2d0ca5e1b74cf11ab798228ceebd542a
Milan Sony <milansonyofficial@gmail.com>
2025-02-25 22:50:36 +0530

    style: 404 page created

---
+++ src/pages/Nopage/Nopage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nopage() {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>Page not found...!</p>
+        </div>
+    )
+}
+
+export default Nopage
