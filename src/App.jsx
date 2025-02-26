cebc0539247b706f1c037c1211852173af0d8613
Milan Sony <milansonyofficial@gmail.com>
2025-02-26 19:51:37 +0530

    template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import Router from './router/Router'
+import { useEffect } from "react";
+import { useWow } from "./hooks/useWow";
+import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useMobileMenu } from "./hooks/useMobileMenu";
+import { useSidebar } from "./hooks/useSidebar";
+import { usePureCounter } from "./hooks/usePureCounter";
+
+import { HomeFour } from "./pages/home/HomeFour";
+import { ErrorPage } from "./pages/error/ErrorPage";
 
 function App() {
+  useWow();
+  useMobileMenu();
+  useSidebar();
+  usePureCounter();
+
+  // on route change to top of the page
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <>
-      <Router />
+      <Routes>
+        <Route path="/" element={<HomeFour />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
